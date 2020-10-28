66f0decddb489ff64d8e6563bda2b18ad9af8879
cmccarthyirl <craig.mccarthy@live.ie>
2020-10-28 23:19:26 +0000

    added local server

---
+++ src/main/java/com/cmccarthyirl/localServer/StartLocalServer.java
@@ -0,0 +1,11 @@
+package com.cmccarthyirl.localServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartLocalServer {
+    public static void main(String[] args) {
+        SpringApplication.run(StartLocalServer.class, args);
+    }
+}
