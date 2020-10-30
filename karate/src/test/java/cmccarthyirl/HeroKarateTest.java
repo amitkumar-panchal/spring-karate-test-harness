72d145bcf9b6cc8419f30e50da7b818ce6e37a2a
cmccarthyirl <craig.mccarthy@live.ie>
2020-10-30 00:00:50 +0000

    cleaned code

--- karate/src/test/java/cmccarthyirl/HeroKarateTest.java
+++ karate/src/test/java/cmccarthyirl/HeroKarateTest.java
@@ -6,6 +6,7 @@ import com.intuit.karate.junit5.Karate;
 
 class HeroKarateTest extends AbstractTestDefinition {
 
+
     @Karate.Test
     Karate testAll() {
         return Karate.run().relativeTo(getClass());
